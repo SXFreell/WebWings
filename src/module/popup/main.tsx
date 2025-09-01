2b78eda47416c8469db3235c0d0be923e4a082cd
Shirosu <xfreell@163.com>
2025-09-01 11:10:28 +0800

    feat: 侧边栏组件开发

--- src/module/popup/main.tsx
+++ src/module/popup/main.tsx
@@ -4,6 +4,8 @@ import { RouterProvider } from 'react-router-dom'
 import '@arco-design/web-react/dist/css/arco.css'
 import router from './router'
 
+import '@/styles/global.less'
+
 const rootElement = document.getElementById('webwings-popup-root')
 if (!rootElement) {
   throw new Error('没有找到webwings-popup-root')
